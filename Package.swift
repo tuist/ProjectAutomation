c69eb65af1f6cc1eb6f1f2df1d993af27470e089
fortmarek <marekfort@me.com>
2022-02-28 22:31:23 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ProjectAutomation",
+    platforms: [.macOS(.v11)],
+    products: [
+        .library(
+            name: "ProjectAutomation",
+            targets: ["ProjectAutomation"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-tools-support-core.git", .upToNextMinor(from: "0.2.0")),
+    ],
+    targets: [
+        .target(
+            name: "ProjectAutomation",
+            dependencies: [
+                .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core")
+            ],
+            path: "tuist/Sources/ProjectAutomation"
+        ),
+    ]
+)
