33e52b2683cf790a48ed41fb92bd7f40673443b4
fortmarek <marekfort@me.com>
2022-07-14 18:49:16 +0200

    Add only ProjectAutomation

--- release.sh
+++ release.sh
@@ -11,7 +11,7 @@ git checkout "$1"
 cd ..
 rm -rf ProjectAutomation
 cp -r tuist/Sources/ProjectAutomation .
-git add .
+git add ProjectAutomation/**
 git commit -am "$1" --allow-empty
 git tag "$1"
 git push origin --follow-tags
